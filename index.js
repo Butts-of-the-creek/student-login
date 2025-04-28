848c9efe6560adf125c10718958284ea3f3e638d
buttsofthecreek <22.malibu.st@gmail.com>
2025-04-28 19:01:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const signUpButton = document.getElementById("signUpButt");
     const signInButton = document.getElementById("signInButt");
     const signInForm = document.getElementById("signInForm");
-    const signUpForm = document.getElementById("firstWrapper")});
+    const signUpForm = document.getElementById("firstWrapper");
 
     signUpButton.addEventListener('click', function () {
         signInForm.style.display = "none";
         signUpForm.style.display = "block";
     });
+
     signInButton.addEventListener('click', function () {
         signInForm.style.display = "block";
         signUpForm.style.display = "none";
-    })
+    });
+});
 
